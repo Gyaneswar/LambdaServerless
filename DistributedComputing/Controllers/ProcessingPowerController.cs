71b31dcbd02a3513c0e2c7ee0187c5845cfddea4
Gyaneswar Singh <gyaneswarsingh53@gmail.com>
2022-05-09 13:16:20 +0530

    Add project files.

---
+++ DistributedComputing/Controllers/ProcessingPowerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DistributedComputing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProcessingPowerController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult Power(int a, int b)
+        {
+            return new JsonResult(a + b);
+        }
+    }
+}
